cfd2df201a95317bee482db62945f0b9c414fd49
Alan Liddell <alan.c.liddell@gmail.com>
2018-03-20 21:28:25 -0400

    adds Account tests, fixes Manager tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,8 +4,19 @@ using Base.Test
 token = ENV["dotok"]
 manager = JuDO.Manager(token)
 
+# load account
+account = get_account(manager)
+@testset "Account" begin
+    @test contains(account.email, "liddell")
+    @test account.droplet_limit == 50
+    @test account.floating_ip_limit == 3
+    @test account.email_verified
+    @test account.status == "active"
+end;
+
 @testset "Manager" begin
     @test manager.ratelimit_limit <= 5000
     @test manager.ratelimit_remaining < manager.ratelimit_limit
-    @test manager.ratelimit_reset < now() + Dates.Hour(1)
 end;
+
+# SSH keys
