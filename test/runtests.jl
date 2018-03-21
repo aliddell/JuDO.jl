c87f55c4adce654edb753d5c7dae62c603254d85
Alan Liddell <alan.c.liddell@gmail.com>
2018-03-20 20:28:47 -0400

    Test nightly builds but don't fail if they fail; test Manager

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,4 +4,8 @@ using Base.Test
 token = ENV["dotok"]
 manager = JuDO.Manager(token)
 
-@test true
+@testset "Manager" begin
+    @test manager.ratelimit_limit <= 5000
+    @test manager.ratelimit_remaining < manager.ratelimit_limit
+    @test manager.ratelimit_reset < now() + Dates.Hour(1)
+end;
